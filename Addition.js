bd3ad3bd5e5f2b88afa028bddf7d6c4d3f6609d0
KingMessi101 <anishbanerjee1000@gmail.com>
2025-06-25 20:00:16 +0930

    Add files via upload

---
+++ Addition.js
@@ -0,0 +1,9 @@
+function sumOfTwoNumbers() {
+    let x, y, z;
+    x= 5;
+    y= 6;
+    z = x + y;
+    return z;
+}
+    let sum=sumOfTwoNumbers();
+    console.log('the sum of x and y is' + sum);
